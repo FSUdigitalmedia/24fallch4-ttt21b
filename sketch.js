99d3c42557c4d8fd01da8ee8839b5e5bec9881a5
TIEN TRUONG <ttt21b@fsu.edu>
2024-09-15 15:48:24 -0400

    initial practice with shapes

--- sketch.js
+++ sketch.js
@@ -1,8 +1,43 @@
+let y = 0; 
+let speed = 3;
+let deg = 0;
+
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER)
 }
 
 function draw() {
-  background(125);
-  circle(width/2, height/2, 40);
+  background(10);
+
+// change color based on vert mouse position
+
+  let startingColor = color("green");
+  let endingColor = color("pink");
+  let pct = map(mouseY, 0, height, 0, 1, true);
+  let clr = lerpColor(startingColor,endingColor,pct);
+  fill(clr);
+
+
+  // draw circle based on mouse
+let d = map(mouseX, 0, width, 50, 150, true);
+let yPos = map2(y, 0, height, 0, height, SINUSOIDAL, IN);
+circle(width/2, yPos, d);
+
+if (y > height || y < 0) {
+  speed = speed * -1;
+}
+
+y = y + speed;
+
+//rotating rectangle
+push();
+translate(100,100);
+rotate (radians(deg));
+deg = (deg + 1) % 360 
+rect(0, 0, 50, 20);
+pop();
+
+
+console.log(d);
 }
